5dd3d04c094528e6613f089e2c214cf3dd410ca7
zdc <3491957218@qq.com>
2025-02-19 22:44:36 +0800

    测试自定义日志格式

--- tests/test_log.cc
+++ tests/test_log.cc
@@ -5,5 +5,8 @@ int main(){
     Logger::ptr logger=LoggerMgr::GetInstance()->getRoot();
     FOCUS_LOG_LEVEL(logger,LogLevel::DEBUG)<<"Test log";
     FOCUS_LOG_FMT_LEVEL(logger,LogLevel::DEBUG,"%s %d","Test log",3);
+    Logger::ptr testLogger = FOCUS_LOG_NAME("test");
+    testLogger->setLogFormatter("[%c]%T[%p]%T[%t]%T[%N]%T[%m]%T%n");
+    FOCUS_LOG_DEBUG(testLogger) << "Test Modfiy Formatter";
     return 0;
 }
